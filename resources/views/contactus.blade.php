35d4b505660270b74ce67ff98170874b7dc3deff
TillWurner <85416653+TillWurner@users.noreply.github.com>
2022-12-06 20:48:57 -0400

    PruebaCss

--- resources/views/contactus.blade.php
+++ resources/views/contactus.blade.php
@@ -5,7 +5,7 @@
    <head>
       <meta charset="utf-8">
       <title>My Diagram C4 | JGM</title>
-      <link rel="stylesheet" href={{ asset("sidebarcss/contactus.css") }}>
+      <link rel="stylesheet" href={{ asset("../sidebarcss/contactus.css") }}>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
    </head>
    <body>
